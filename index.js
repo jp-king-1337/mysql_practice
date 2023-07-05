ad2d782ab6ced7b34d4c6ff454fa5f4c89fb6e85
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-05 14:37:20 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'books_db'
+  });
+
+  connection.query("SELECT * FROM biographies", (err, data) => {
+    if (err) return console.log(err);
+
+    console.log(data);
+  });

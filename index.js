a292c801231a18ee1c5334c4accafd693fb0f8b5
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-06 15:21:45 -0400

    Added heroku server

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
+const express = require("express");
 const mysql = require("mysql2");
-const inquirer = require("inquirer");
-const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+const isProduction = process.env.PORT;
 const connectionString = "mysql://kyoslnhxriwjsi6p:b8r4f8sbj1w8t3pq@acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pu18reymzbigidh6";
-
-
 
 const connection = mysql.createConnection(isProduction ? connectionString : {
   host: "127.0.0.1",
   user: "root",
-  database: "books_db"
+  database: "books_db",
+  multipleStatements: "true"
 });
 
-function printAllBios() {
-  connection.query("SELECT * FROM biographies", (err, data) => {
-    if (err) return console.log(err);
+app.get("/", (clientReq, serverRes) => {
+  const tableSQL = `
+  DROP TABLE IF EXISTS biographies;
+  CREATE TABLE biographies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250),
+    author VARCHAR(250),
+  );
 
-    console.log(data);
-    connection.end();
-  });
-}
+  INSERT INTO biographies (name, author) VALUES
+    ("Titanic", "JP"),
+    ("Another", "JP"),
+    ("One More", "JP");
+  `;
 
-inquirer.prompt([
-  {
-    name: "title",
-    message: "Please provide a title for the biography."
-  },
-  {
-    name: "author",
-    message: "Please enter the author name."
-  }
-]).then((answerObj) => {
-  // Query our db and insert a new row into the biographies table
-  const sql = "INSERT INTO biographies (name, author) VALUES (?, ?)";
+  connection.query(tableSQL, (err) => {
+    if (err) throw err;
 
-  connection.query(sql, [answerObj.title, answerObj.author], (err, result) => {
-    if (err) return console.log(err);
-    console.log("New bio with id of %s added successfully!\n", result.insertId);
+    connection.query("SELECT * FROM biographies", (err, data) => {
+      if (err) return console.log(err);
 
-    printAllBios();
-  });
+      serverRes.send(data);
+    });
+  })
 });
+
+
+app.listen(PORT, () => console.log("Server started."));

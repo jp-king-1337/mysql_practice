f28f340fdb624342c0e8dc7a21fa1f2ea73a0f8b
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-06 12:20:07 -0400

    Almost lunch time

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,15 @@
-DROP DATABASE IF EXISTS example_mysql_db;
-CREATE DATABASE example_mysql_db;
+DROP DATABASE IF EXISTS books_db;
+CREATE DATABASE books_db;
+
+USE books_db;
+
+CREATE TABLE biographies (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(250),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO biographies (name) VALUES
+    ("Titanic"),
+    ("Another"),
+    ("One More");
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-const mysql = require('mysql2');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'books_db'
+  host: "localhost",
+  user: "root",
+  database: "books_db"
 });
 
-connection.query('SELECT * FROM biographies', (err, data) => {
+connection.query("SELECT * FROM biographies", (err, data) => {
   if (err) return console.log(err);
 
   console.log(data);
 });
+
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        message: "Enter the title of a biography:",
+        validate: value => (value ? true : "Please enter the title of a biography.")
+      }
+    ])
+    .then(answers => {
+      const sql = `INSERT INTO biographies (name) VALUES ("${answers.title}")`;
+      connection.query(sql, (err, result) => {
+        if (err) return console.log(err);
+
+        connection.query("SELECT * FROM biographies", (err, data) => {
+          if (err) return console.log(err);
+
+          console.log("Biography added!");
+          console.log(data);
+          connection.end();
+        });
+      });
+    });
+}
+
+promptUser();

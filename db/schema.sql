5ecba376715c1fa14c07d1a7cb8f6fd1c03ef5b7
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 15:25:35 -0400

    If main won't update, I'll copy the code directly from heroku-deploy branch on GitHub and paste it into main branch on my computer then commit and push from there

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,15 @@
--- Drop or delete the database to reset the data - erase all data/tables
 DROP DATABASE IF EXISTS books_db;
--- Create our books_db database
 CREATE DATABASE books_db;
 
 USE books_db;
 
 CREATE TABLE biographies (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(250),
-  author VARCHAR(250),
-  PRIMARY KEY(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250),
+    author VARCHAR(250),
 );
 
-INSERT INTO biographies (name, author) VALUES 
-  ('Titanic', 'JD'),
-  ('Another', 'JD'),
-  ('One More', 'JD');
+INSERT INTO biographies (name, author) VALUES
+    ("Titanic", "JP"),
+    ("Another", "JP"),
+    ("One More", "JP");

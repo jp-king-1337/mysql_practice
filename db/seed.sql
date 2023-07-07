77637c15338b82c70ef5f4c79df4caf9790d06d7
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 11:59:16 -0400

    Went back to generic names and projects, updating tab spacing

--- db/seed.sql
+++ db/seed.sql
@@ -5,106 +5,106 @@ CREATE DATABASE project_tracker_db;
 USE project_tracker_db;
 
 CREATE TABLE students (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL
 );
 
 INSERT INTO students (first_name, last_name) VALUES
-  ("Sophie", "Scholl"),
-  ("Hans", "Scholl"),
-  ("Alexander", "Schmorell"),
-  ("Christoph", "Probst"),
-  ("Willi", "Graf"),
-  ("Kurt", "Huber"),
-  ("Carl", "Friedrich Goerdeler"),
-  ("Marie", "Lange"),
-  ("Elisabeth", "Schumacher"),
-  ("Paul", "Reckzeh");
+    ("John", "Doe"),
+    ("Jane", "Smith"),
+    ("Michael", "Johnson"),
+    ("Emily", "Davis"),
+    ("Daniel", "Wilson"),
+    ("Olivia", "Thompson"),
+    ("William", "Anderson"),
+    ("Sophia", "Thomas"),
+    ("James", "Roberts"),
+    ("Ava", "Taylor");
 
 CREATE TABLE projects (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  project_name VARCHAR(250) NOT NULL,
-  completed BOOLEAN DEFAULT true,
-  student_id INT NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES students(id)
-    ON DELETE CASCADE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(250) NOT NULL,
+    completed BOOLEAN DEFAULT true,
+    student_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+        ON DELETE CASCADE
 );
 
 INSERT INTO projects (project_name, completed, student_id) VALUES
-    -- Student 1 (Sophie Scholl)
-    ("Leaflet Distribution", true, 1),
-    ("Resistance Pamphlets", false, 1),
-    ("Literary Circle", true, 1),
-    ("Anti-Nazi Propaganda", false, 1),
-    ("Underground Printing Press", true, 1),
+    -- Student 1 (John Doe)
+    ("Clock Project", true, 1),
+    ("Clock Project Version 2.0", false, 1),
+    ("Book Recommendation System", true, 1),
+    ("E-commerce Website", false, 1),
+    ("Music Streaming App", true, 1),
 
-    -- Student 2 (Hans Scholl)
-    ("Resistance Calendar", false, 2),
-    ("Protest Posters", true, 2),
-    ("Information Analysis", false, 2),
-    ("Clandestine Food Network", true, 2),
-    ("Covert Financial Tracking", false, 2),
+    -- Student 2 (Jane Smith)
+    ("Calendar Application", false, 2),
+    ("Clock Project Version 2.0", true, 2),
+    ("Social Media Analytics", false, 2),
+    ("Food Delivery App", true, 2),
+    ("Expense Tracker", false, 2),
 
-    -- Student 3 (Alexander Schmorell)
-    ("Codebreaking System", true, 3),
-    ("Secret Messaging App", false, 3),
-    ("Black Market Operations", true, 3),
-    ("Resistance Task Management", false, 3),
-    ("Weather Intelligence", true, 3),
+    -- Student 3 (Michael Johnson)
+    ("Chatbot Development", true, 3),
+    ("Fitness Tracking App", false, 3),
+    ("Online Marketplace", true, 3),
+    ("Task Management System", false, 3),
+    ("Weather Forecasting App", true, 3),
 
-    -- Student 4 (Christoph Probst)
-    ("Underground Food Distribution", true, 4),
-    ("Expense Tracking System", false, 4),
-    ("Resistance Chatbot", true, 4),
-    ("Fitness Training Program", false, 4),
-    ("Covert Online Marketplace", true, 4),
+    -- Student 4 (Emily Davis)
+    ("Food Delivery App", true, 4),
+    ("Expense Tracker", false, 4),
+    ("Chatbot Development", true, 4),
+    ("Fitness Tracking App", false, 4),
+    ("Online Marketplace", true, 4),
 
-    -- Student 5 (Willi Graf)
-    ("Secret E-commerce Network", false, 5),
-    ("Underground Music Streaming", true, 5),
-    ("Resistance Calendar", false, 5),
-    ("Clockwork Sabotage", true, 5),
-    ("Undercover Social Media Analysis", false, 5),
+    -- Student 5 (Daniel Wilson)
+    ("E-commerce Website", false, 5),
+    ("Music Streaming App", true, 5),
+    ("Calendar Application", false, 5),
+    ("Clock Project Version 2.0", true, 5),
+    ("Social Media Analytics", false, 5),
 
-    -- Student 6 (Kurt Huber)
-    ("Covert Music Streaming App", true, 6),
-    ("Calendar Encryption", false, 6),
-    ("Stealthy Clock Design", true, 6),
-    ("Underground Social Media Analysis", false, 6),
-    ("Expense Tracking System", true, 6),
+    -- Student 6 (Olivia Thompson)
+    ("Music Streaming App", true, 6),
+    ("Calendar Application", false, 6),
+    ("Clock Project", true, 6),
+    ("Social Media Analytics", false, 6),
+    ("Expense Tracker", true, 6),
 
-    -- Student 7 (Carl Friedrich Goerdeler)
-    ("Chatbot Intelligence", false, 7),
-    ("Covert Fitness Tracking", true, 7),
-    ("Resistance Marketplace", false, 7),
-    ("Underground Task Management", true, 7),
-    ("Weather Forecasting Network", false, 7),
+    -- Student 7 (William Anderson)
+    ("Chatbot Development", false, 7),
+    ("Fitness Tracking App", true, 7),
+    ("Online Marketplace", false, 7),
+    ("Task Management System", true, 7),
+    ("Weather Forecasting App", false, 7),
 
-    -- Student 8 (Marie Lange)
-    ("Book Recommendations", true, 8),
-    ("Covert E-commerce Network", false, 8),
-    ("Resistance Music Streaming", true, 8),
-    ("Underground Calendar App", false, 8),
-    ("Stealthy Social Media Analysis", true, 8),
+    -- Student 8 (Sophia Thomas)
+    ("Book Recommendation System", true, 8),
+    ("E-commerce Website", false, 8),
+    ("Music Streaming App", true, 8),
+    ("Calendar Application", false, 8),
+    ("Social Media Analytics", true, 8),
 
-    -- Student 9 (Elisabeth Schumacher)
-    ("Expense Tracking System", false, 9),
-    ("Resistance Chatbot", true, 9),
-    ("Fitness Training App", false, 9),
-    ("Underground Marketplace", true, 9),
-    ("Covert Task Management", false, 9),
+    -- Student 9 (James Roberts)
+    ("Expense Tracker", false, 9),
+    ("Chatbot Development", true, 9),
+    ("Fitness Tracking App", false, 9),
+    ("Online Marketplace", true, 9),
+    ("Task Management System", false, 9),
 
-    -- Student 10 (Paul Reckzeh)
-    ("Weather Forecasting Network", true, 10),
-    ("Book Recommendations", false, 10),
-    ("Covert E-commerce Network", true, 10),
-    ("Resistance Music Streaming", false, 10),
-    ("Calendar Encryption", true, 10);
+    -- Student 10 (Ava Taylor)
+    ("Weather Forecasting App", true, 10),
+    ("Book Recommendation System", false, 10),
+    ("E-commerce Website", true, 10),
+    ("Music Streaming App", false, 10),
+    ("Calendar Application", true, 10);
 
 SELECT
     CONCAT(first_name, " ", last_name) AS student_name,
     project_name,
     completed
-        FROM students
-        JOIN projects ON students.id = projects.student_id;
+    FROM students
+    JOIN projects ON students.id = projects.student_id;

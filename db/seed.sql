a0c9cb4be6e2837b83d8cb61033656a69b54ac63
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 11:23:11 -0400

    Added seed.sql for the activity, added students and projects into it.

--- db/seed.sql
+++ db/seed.sql
@@ -6,9 +6,22 @@ USE project_tracker_db;
 
 CREATE TABLE students (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(250),
-  last_name VARCHAR(250)
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL
 );
+
+INSERT INTO students (first_name, last_name) VALUES
+  ("John", "Doe"),
+  ("Jane", "Smith"),
+  ("Michael", "Johnson"),
+  ("Emily", "Davis"),
+  ("Daniel", "Wilson"),
+  ("Olivia", "Thompson"),
+  ("William", "Anderson"),
+  ("Sophia", "Thomas"),
+  ("James", "Roberts"),
+  ("Ava", "Taylor");
+
 
 CREATE TABLE projects (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -18,3 +31,74 @@ CREATE TABLE projects (
   FOREIGN KEY student_id REFERENCES students(id)
     ON DELETE CASCADE
 );
+
+INSERT INTO projects (project_name, completed, student_id) VALUES
+    -- Student 1
+    ("Clock Project", true, 1),
+    ("Clock Project Version 2.0", false, 1),
+    ("Book Recommendation System", true, 1),
+    ("E-commerce Website", false, 1),
+    ("Music Streaming App", true, 1),
+
+    -- Student 2
+    ("Calendar Application", false, 2),
+    ("Clock Project Version 2.0", true, 2),
+    ("Social Media Analytics", false, 2),
+    ("Food Delivery App", true, 2),
+    ("Expense Tracker", false, 2),
+
+    -- Student 3
+    ("Chatbot Development", true, 3),
+    ("Fitness Tracking App", false, 3),
+    ("Online Marketplace", true, 3),
+    ("Task Management System", false, 3),
+    ("Weather Forecasting App", true, 3),
+
+    -- Student 4
+    ("Food Delivery App", true, 4),
+    ("Expense Tracker", false, 4),
+    ("Chatbot Development", true, 4),
+    ("Fitness Tracking App", false, 4),
+    ("Online Marketplace", true, 4),
+
+    -- Student 5
+    ("E-commerce Website", false, 5),
+    ("Music Streaming App", true, 5),
+    ("Calendar Application", false, 5),
+    ("Clock Project Version 2.0", true, 5),
+    ("Social Media Analytics", false, 5),
+
+    -- Student 6
+    ("Music Streaming App", true, 6),
+    ("Calendar Application", false, 6),
+    ("Clock Project", true, 6),
+    ("Social Media Analytics", false, 6),
+    ("Expense Tracker", true, 6),
+
+    -- Student 7
+    ("Chatbot Development", false, 7),
+    ("Fitness Tracking App", true, 7),
+    ("Online Marketplace", false, 7),
+    ("Task Management System", true, 7),
+    ("Weather Forecasting App", false, 7),
+
+    -- Student 8
+    ("Book Recommendation System", true, 8),
+    ("E-commerce Website", false, 8),
+    ("Music Streaming App", true, 8),
+    ("Calendar Application", false, 8),
+    ("Social Media Analytics", true, 8),
+
+    -- Student 9
+    ("Expense Tracker", false, 9),
+    ("Chatbot Development", true, 9),
+    ("Fitness Tracking App", false, 9),
+    ("Online Marketplace", true, 9),
+    ("Task Management System", false, 9),
+
+    -- Student 10
+    ("Weather Forecasting App", true, 10),
+    ("Book Recommendation System", false, 10),
+    ("E-commerce Website", true, 10),
+    ("Music Streaming App", false, 10),
+    ("Calendar Application", true, 10);

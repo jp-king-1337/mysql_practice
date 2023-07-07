3028eaf1ef1bdecfc5ed13580c1760b18e792f22
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 14:33:17 -0400

    Updated to JD's code to follow along in class

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,18 @@
-DROP DATABASE IF EXISTS student_app_db;
-CREATE DATABASE student_app_db;
+-- Drop or delete the database to reset the data - erase all data/tables
+DROP DATABASE IF EXISTS books_db;
+-- Create our books_db database
+CREATE DATABASE books_db;
+
+USE books_db;
+
+CREATE TABLE biographies (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(250),
+  author VARCHAR(250),
+  PRIMARY KEY(id)
+);
+
+INSERT INTO biographies (name, author) VALUES 
+  ('Titanic', 'JD'),
+  ('Another', 'JD'),
+  ('One More', 'JD');

6c0ebdd99a76bf86174649ee1ad9225d27fc5fbb
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-12 14:44:55 -0400

    Finished with class, fixed errors

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ const PORT = process.env.PORT || 3333;
 
 // Middleware
 app.use(express.json()); // Allows the client/browser to send json in a request
+// Allow standard encoded form data submissions
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); // Allows the client/browser to access any folders or files in public - opens this folder at the root
 
 // Setup Handlebars Template Engine
 app.engine("hbs", engine({
     // layout directory that allows you to avoid repeated html code
     layoutsDir: "./views/layouts",
-    // Set the extension for handlebars files to hbs
+    // Set the extension for your handlebars files to .hbs
     extname: "hbs"
 }));
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
 // Load Routes
-app.use("/", [api_routes, view_routes]);
+app.use("/", [api_routes, view_routes, user_routes]);
 
 // Connect to the db and create all tables based off of our models
 db.sync({ force: false })

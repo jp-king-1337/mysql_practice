3104e25dfd07e8e2eb49f092aa168c7f01b6dad5
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-13 16:24:37 -0400

    updated formatting in connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,15 +3,15 @@ const { Sequelize } = require("sequelize");
 const isProduction = process.env.JAWSDB_URL;
 let sequelize;
 
-// Trying heroku again
-
-if (isProduction)
+if (isProduction) {
     sequelize = new Sequelize(process.env.JAWSDB_URL, {
         dialect: "mysql"
     });
-else sequelize = new Sequelize("user_app_example", "root", "", {
-    host: "localhost",
-    dialect: "mysql"
-});
+} else {
+    sequelize = new Sequelize("user_app_example", "root", "", {
+        host: "localhost",
+        dialect: "mysql"
+    });
+}
 
 module.exports = sequelize;
